3dc0a5a06866759bfc825901109e2891316a83f4
Ettore M. G. Trainiti <ettore.trainiti@gmail.com>
2017-05-03 17:16:44 -0500

    Added script to batch generate outputs given a dataset

--- gen_output.py
+++ gen_output.py
@@ -5,29 +5,29 @@ def gen_output(data_set):
 
     file_list = glob.glob("../data/r1.0/models/*/")
 
-    for gen_for in file_list:
+    counter = 1
 
-        counter = 0
+    for gen_for in file_list:
 
         to_call = "python gen_new_model.py --emb_size 300 --state_size 300 --max_grad_nor 5.0 "
         to_call+= "--test_on " + data_set + " "
         to_call+= "--new_model_path " + gen_for + " "
         # print to_call
 
-        tmux = "tmux new -s " + "run_" + str(counter) + " -d "
-        tmux+= "'"
-        tmux+="source ~/Env/tensorflow-r1.0;"
-        tmux+=to_call
-        tmux+="'"
+        # tmux = "tmux new -s " + "run_" + str(counter) + " -d "
+        # tmux+= "'"
+        # tmux+="source ~/Env/tensorflow-r1.0;"
+        # tmux+=to_call
+        # tmux+="'"
+        #
+        # counter +=1
 
-        counter +=1
-
-        os.system(tmux)
+        os.system(to_call)
 
     return
 
 if __name__ == "__main__":
     print
-    gen_output("valid")
+    gen_output("train")
     print
 
